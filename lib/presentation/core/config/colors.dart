25a35d0f5870d5be0ee4c61d06c6baf9843d957c
kimiya zargari <kimiyazargari13@gmail.com>
2023-03-03 19:54:43 +0330

    update theme

--- lib/presentation/core/config/colors.dart
+++ lib/presentation/core/config/colors.dart
@@ -5,4 +5,5 @@ class AppColors {
 
   static const darkBackground = Color(0xff222222);
   static const darkCardColor = Color(0xff282828);
+  static const primaryColor = Color(0xffd2afff);
 }
--- lib/presentation/core/config/themes/dark_theme.dart
+++ lib/presentation/core/config/themes/dark_theme.dart
@@ -12,7 +12,7 @@ TextTheme _textTheme(TextTheme base) {
         fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white),
 
     labelLarge: const TextStyle(
-        fontSize: 22, fontWeight: FontWeight.w500, color: Colors.white),
+        fontSize: 18, fontWeight: FontWeight.w500, color: Colors.white),
 
     labelMedium: const TextStyle(
         fontSize: 14, fontWeight: FontWeight.w400, color: Colors.white),
@@ -43,49 +43,57 @@ TextTheme _primaryTextTheme(TextTheme base) {
 ThemeData darkTheme() {
   final ThemeData base = ThemeData.dark();
   return base.copyWith(
-    scaffoldBackgroundColor: AppColors.darkBackground,
-    textTheme: _textTheme(base.textTheme),
-    primaryTextTheme: _primaryTextTheme(base.textTheme),
-    primaryColorDark: Colors.deepPurple,
-    primaryColor: Colors.deepPurpleAccent,
-    colorScheme: base.colorScheme.copyWith(primary: Colors.deepPurpleAccent),
-  //  focusColor: Colors.deepPurpleAccent,
-    cardTheme: base.cardTheme.copyWith(
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      textTheme: _textTheme(base.textTheme),
+      primaryTextTheme: _primaryTextTheme(base.textTheme),
+      primaryColorDark: Colors.deepPurple,
+      primaryColor: AppColors.primaryColor,
+      colorScheme: base.colorScheme.copyWith(
+        primary: AppColors.primaryColor,
+      ),
+      //  focusColor: Colors.deepPurpleAccent,
+      cardTheme: base.cardTheme.copyWith(
+          elevation: 0,
+          color: AppColors.darkCardColor,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12)))),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+        textStyle: _textTheme(base.textTheme).labelLarge,
+        backgroundColor: AppColors.primaryColor,
         elevation: 0,
-        color: AppColors.darkCardColor,
+        disabledBackgroundColor: Colors.grey,
+        padding: const EdgeInsets.symmetric(vertical: 10),
         shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(12)))),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-      backgroundColor: Colors.deepPurpleAccent,
-      elevation: 0,
-      disabledBackgroundColor: Colors.grey,
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-      ),
-    )),
-    outlinedButtonTheme: OutlinedButtonThemeData(
-        style: OutlinedButton.styleFrom(
-      elevation: 0,
-      disabledBackgroundColor: Colors.grey,
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-      ),
-    )),
-    iconTheme: base.iconTheme.copyWith(
-      color: Colors.white,
-    ),
-    floatingActionButtonTheme: base.floatingActionButtonTheme.copyWith(
-        backgroundColor: Colors.deepPurpleAccent,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+      )),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
         elevation: 0,
-        foregroundColor: Colors.white),
-    appBarTheme: base.appBarTheme.copyWith(
-        color: Colors.deepPurple,
-        centerTitle: true,
-        elevation: 0,
-        foregroundColor: Colors.white),
-    dialogTheme: base.dialogTheme.copyWith(backgroundColor: AppColors.darkCardColor, )
-  );
+        foregroundColor: AppColors.primaryColor,
+        disabledBackgroundColor: Colors.grey,
+        textStyle: _textTheme(base.textTheme).labelLarge,
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+      )),
+      iconTheme: base.iconTheme.copyWith(
+        color: Colors.white,
+      ),
+      floatingActionButtonTheme: base.floatingActionButtonTheme.copyWith(
+          backgroundColor: AppColors.primaryColor,
+          elevation: 0,
+          foregroundColor: AppColors.darkCardColor),
+      appBarTheme: base.appBarTheme.copyWith(
+          color: AppColors.primaryColor,
+          centerTitle: true,
+          elevation: 0,
+          foregroundColor: AppColors.darkCardColor),
+      dialogTheme: base.dialogTheme.copyWith(
+          backgroundColor: AppColors.darkCardColor,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          )));
 }
