d913eedc1209e7f9b64bfaf1628987485abcde0d
kimiya zargari <kimiyazargari13@gmail.com>
2023-03-09 14:14:49 +0330

    update background color in light mode

--- lib/presentation/core/config/colors.dart
+++ lib/presentation/core/config/colors.dart
@@ -4,10 +4,10 @@ class AppColors {
   AppColors._();
 
   static const darkBackground = Color(0xff191915);
-  static const lightBackground = Color(0xffa8a98c);
+  static const lightBackground = Color(0xffe6e9ca);
   static const darkCardColor = Color(0xff33332a);
-  static const lightCardColor = Color(0xff99987d);
-  static const primaryColor = Color(0xff97a973);
+  static const lightCardColor = Color(0xff99997d);
+  static const primaryColor = Color(0xff97ac73);
   static const primaryColor2 = Color(0xff99987d);
   static const error = Colors.orangeAccent;
   static const darkThemeTextColor =  Color(0xfff6f4eb);
